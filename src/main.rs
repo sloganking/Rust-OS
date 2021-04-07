81ab8787f55b4f363404ca2039fb644fb9b92e79
Logan King <seanloganking@gmail.com>
2021-04-07 01:38:54 -0400

    global writer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// src/main.rs
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -11,19 +11,12 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// print hello world to the screen
-    static HELLO: &[u8] = b"Hello World!";
-
+// print to the screen
     #[no_mangle]
     pub extern "C" fn _start() -> ! {
-        let vga_buffer = 0xb8000 as *mut u8;
-
-        for (i, &byte) in HELLO.iter().enumerate() {
-            unsafe {
-                *vga_buffer.offset(i as isize * 2) = byte;
-                *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-            }
-        }
-
+        use core::fmt::Write;
+        vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+        write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    
         loop {}
     }
