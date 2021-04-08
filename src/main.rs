9d8f36dc9c0a545b514526869cc973d8334c3e42
Logan King <seanloganking@gmail.com>
2021-04-07 21:22:30 -0400

    trivial_assertion()
    running first test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -12,12 +15,28 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 // print to the screen
     #[no_mangle]
-    pub extern "C" fn _start() {
+    pub extern "C" fn _start() -> ! {
         println!("Hello World{}", "!");
-        panic!("Some panic message");
-        println!("Yeet!");
+
+        #[cfg(test)]
+        test_main();
 
         loop {}
     }
